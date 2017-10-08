b33a7e09811fb143cdc5e489d4167d4f22b3bc05
JulienToons <32437624+JulienToons@users.noreply.github.com>
2017-10-08 08:51:34 -0700

    Background.1
    error w. background though

--- TouchWindow.java
+++ TouchWindow.java
@@ -3,13 +3,26 @@ import java.awt.*;
 
 public class TouchWindow extends WindowController
 {
+	private Location location;
+	private FilledRect background = new FilledRect(0,0,canvas.getWidth(),canvas.getHeight(),canvas);
+	private Color back = new Color(255,255,255);
 	public void begin()
 	{
-		EasyReader console = new EasyReader();
-		FilledRect joe;
-		joe = new FilledRect(10,10,100,200,canvas);
-		joe.setColor(Color.blue);
-		console.readLine();
+		background.setColor(back);
+		FilledRect start;
+		start = new FilledRect(10,10,10,10,canvas);
+		start.setColor(Color.white);
 	}
+	public void onMouseExit(Location point)
+	{
+		
+	}
+	public void onMousePress(Location pressPoint)
+    {
+		location = pressPoint;
+	}
+	// public static void button();
+	// {
 
-}
+	// }
+}//pongback
